c9c2c0b1a14ab49e1171b22ad1aae59e260e1668
Sean Lao <slao258@gmail.com>
2015-09-07 16:45:46 -0700

    add initial test

---
+++ Calculator.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace calculator
+{
+	class Calculator
+	{
+		public int Compute(string expr)
+		{
+			return 0;
+		}
+	}
+
+}
+
---
+++ MainClass.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace calculator
+{
+	public class MainClass
+	{
+		public static void Main()
+		{
+			RunTests();
+		}
+
+		private static void RunTests()
+		{
+			Calculator c = new Calculator ();
+			c.Compute ("1 + 1");
+		}
+	}
+}
+
